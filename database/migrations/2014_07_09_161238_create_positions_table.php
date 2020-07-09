ea37c06cd5ea1324c7591f8d59e82948c3e1499f
makara deu <makara.deu99@gmail.com>
2020-07-10 06:49:45 +0700

    #9 create migrations file into database

---
+++ database/migrations/2014_07_09_161238_create_positions_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePositionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('positions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('position');
+            $table->timestamps();
+        });
+        
+        // Insert default position
+        DB::table('positions')->insert(
+            array(
+                'id'=>1, // 1: admin 2 : HR 3: manager 4 : employee
+                'position'=> 'Admin'
+            )
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('positions');
+    }
+}
---
+++ database/migrations/2019_10_12_000000_create_users_table.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('firstName');
+            $table->string('lastName');
+            $table->string('email')->unique();
+            $table->integer('role')->default(4); // 1 : admin 2 : HR 3: manager 4 : employee
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->date('startDate')->date('Y-m-d H:i:s');
+            $table->string('profile');
+            $table->integer('position_id')->unsigned();
+            $table->foreign('position_id')
+                  ->references('id')
+                  ->on('positions')
+                  ->onDelete('cascade');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+        
+        // Insert default user as admin 
+        DB::table('users')->insert(
+            array(
+                'id'=>1,
+                'firstName'=>'Admin',
+                'lastName'=>'User',
+                'email'=>'admin@gmail.com',
+                'role'=>1,
+                'password'=>bcrypt('password'),
+                'startDate'=>'2020-07-10',
+                'profile'=>'profile.png',
+                'position_id'=>1,
+                'remember_token' => Str::random(10)
+            )
+            );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
