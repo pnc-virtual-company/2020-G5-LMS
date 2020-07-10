56dfa4471ec4a4d15b0001b2883141b60ba5c73b
sopheaktra <sopheaktra.ros@student.passerellesnumeriques.org>
2020-07-10 18:46:24 +0700

    #14 doing department view

--- resources/views/pages/department/showDepartment.blade.php
+++ resources/views/pages/department/showDepartment.blade.php
@@ -2,6 +2,67 @@
 
 @section('content')
 
-<p>Department viewy</p>
- 
+<div class="container">
+
+    <div class="row">
+        <div class="col-2"></div>
+        <div class="col-8">
+            <input class="form-control" id="myInput" type="text" placeholder="Search..">
+        </div>
+        <div class="col-2"></div>
+    </div>
+
+    <div class="row mt-5">
+        <div class="col-2"></div>
+        <div class="col-4"><h1>Department</h1></div>
+        <div class="col-4">
+            <a href="#" class="btn btn-warning float-right text-white btn-lg" style="border-radius: 25px; "><strong>+ Create</strong></a>
+        </div>
+        <div class="col-2"></div>
+    </div>
+    <div class="row">
+        <div class="col-2"></div>
+        <div class="col-8">
+            <table class="table table-borderless table-hover">
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Training and education team</td>
+                        <td class="float-right" >
+                            <a href="#"><span class="material-icons text-secondary">edit</span></a>
+                            <a href="#"><span class="material-icons text-secondary">delete</span></a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>External relation team</td>
+                        <td class="float-right">
+                            <a href="#"><span class="material-icons text-secondary">edit</span></a>
+                            <a href="#"><span class="material-icons text-secondary">delete</span></a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Admin and finance team</td>
+                        <td class="float-right">
+                            <a href="#"><span class="material-icons text-secondary">edit</span></a>
+                            <a href="#"><span class="material-icons text-secondary">delete</span></a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Section team</td>
+                        <td class="float-right">
+                            <a href="#"><span class="material-icons text-secondary">edit</span></a>
+                            <a href="#"><span class="material-icons text-secondary">delete</span></a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="col-2"></div>
+    </div>
+</div>
 @endsection
