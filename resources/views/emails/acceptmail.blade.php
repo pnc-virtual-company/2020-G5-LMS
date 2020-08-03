29feab347293ad016bce945242c67f5d217943d6
makara deu <makara.deu99@gmail.com>
2020-08-03 13:34:36 +0700

    #46 Add accept and reject style css

--- resources/views/emails/acceptmail.blade.php
+++ resources/views/emails/acceptmail.blade.php
@@ -4,33 +4,45 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
     <title>Document</title>
+    <style>
+        .container{
+            margin-left: 20px;
+        }
+        table {
+          border-collapse: collapse;
+          width: 70%;
+        }
+        
+        table, td {
+          border: 1px solid black;
+          padding: 10px;
+        }
+        </style>
 </head>
 <body>
-    <div class="container">
-
-        <div class="row">
-            <div class="col-12">
-                <h4>Leave Request</h4>
-                <p>Dear {{ $accepts['firstName'] }} {{ $accepts['lastName'] }}</p>
-                <br>
-                <p>This time off your request has been approved.</p>
-            </div>
-        </div>
-
-        <div class="row">
-
-            <div class="col-12">
-                <p>From         {{ $accepts['startDate'] }}</p>
-                <p>To           {{ $accepts['endDate'] }}</p>
-                <p>Type         {{ $accepts['type'] }}</p>
-                <p>Reason       {{ $accepts['type'] }}</p>
-                <p>Last comment {{ $accepts['comment'] }}</p>
-            </div>
-
-        </div>
-
-    </div>
+   <div class="container">
+       <h4>Leave Request</h4>
+       <p>Dear {{ $accepts['firstName'] }} {{ $accepts['lastName'] }}</p>
+       <p>The time off you requested has been approved.</p>
+       <table>
+           <tr>
+               <td>From</td>
+               <td>{{ $accepts['startDate'] }}</td>
+           </tr>
+           <tr>
+               <td>To</td>
+               <td> {{ $accepts['endDate'] }}</td>
+           </tr>
+           <tr>
+               <td>Type</td>
+               <td>{{ $accepts['type'] }}</td>
+           </tr>
+           <tr>
+               <td>Last Comment</td>
+               <td>{{ $accepts['comment'] }}</td>
+           </tr>
+       </table>
+   </div>
 </body>
 </html>
